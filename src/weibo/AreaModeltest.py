675f7dee0e167c637a7302359d09b129688e9089
dusaiya <dusaiya@126.com>
2017-12-05 16:44:45 +0800

    增加程序

---
+++ src/weibo/AreaModeltest.py
@@ -0,0 +1,80 @@
+# encoding: utf-8
+'''
+Created on 2017年11月28日
+
+@author: alibaba
+'''
+import MySQLdb
+import random
+
+def dbInfo():
+    conn = MySQLdb.connect(host='10.60.1.73', port=3306, user='shenhuawei',
+             passwd='123456', db='test2', charset="utf8")
+    cur = conn.cursor()
+    return conn, cur
+
+def get_uidlist(conn,cur):
+    uidct = cur.execute("select uid from uid_city_over_3")
+    dts = cur.fetchmany(uidct)
+    conn.commit()
+    uidlist=[]
+    for dt in dts:
+        uidlist.append(str(dt[0]))
+    return uidlist
+
+def get_weibolist(uid,conn,cur,preDay,sufDay):
+    '''
+    自动获取80%的数据
+    '''
+    weiboct = cur.execute("select msg_city from weibo where uid=%s "+" and msg_city<>'' and msg_city<>'" + 
+        "白山".decode("utf8") + "' and real_date>=%s and real_date<%s",
+                          (uid,preDay,sufDay))
+    dts = cur.fetchmany(weiboct)
+    conn.commit()
+    tmp_list=[]
+    for dt in dts:
+        tmp_list.append(str(dt[0].encode("utf-8")))
+    return random.sample(tmp_list,len(tmp_list)*8/10),uid
+    #return tmp_list,uid
+
+def uid_extract_save(uidlist,ct,filestr):
+    new_uidlist=random.sample(uidlist,ct)
+    fout=open("./data/uidlist_"+filestr,"wb")
+    for uid in new_uidlist:
+        fout.write(uid)
+        fout.write("\n")
+    fout.close()
+    return new_uidlist
+
+def weibo_get_save(uidlist, conn, cur, preDay, sufDay,filestr):
+    print "length of uidlist_"+ filestr+":"+ str(len(uidlist_a))
+    i=0
+    fout=open("./data/weibolist_"+filestr,"wb")
+    for uid in uidlist:
+        weibolist,uid=get_weibolist(uid, conn, cur, preDay, sufDay)
+        for weibo in weibolist:
+            fout.write(uid+"\t"+weibo+"\n")
+            i+=1
+            if i%10000==0:
+                print i
+    fout.close()
+    print "length of weibolist_"+ filestr+":"+ str(i)
+
+if __name__ == "__main__":
+    conn,cur=dbInfo()
+    uidlist=get_uidlist(conn,cur)
+    batchstr="3"
+    batchtype="half"
+    seta="a"
+    setb="b"
+    
+    uidlist_a=uid_extract_save(uidlist,2500,batchtype+"_"+seta+"_"+batchstr) # 2500 大约 75%
+    uidlist_b=uid_extract_save(uidlist,2500,batchtype+"_"+setb+"_"+batchstr) 
+    preDay="2016-06-01"
+    sufDay="2016-06-16"
+    weibo_get_save(uidlist_a, conn, cur, preDay, sufDay,batchtype+"_"+seta+"_"+batchstr)
+    preDay="2016-06-01"
+    sufDay="2016-06-16"
+    weibo_get_save(uidlist_b, conn, cur, preDay, sufDay,batchtype+"_"+setb+"_"+batchstr)
+    
+    
---
+++ src/weibo/Bload_area.py
@@ -0,0 +1,80 @@
+# encoding: utf-8
+'''
+Created on 2017年11月29日
+
+@author: alibaba
+
+'''
+import numpy as np
+
+def loaddata(fpath,batchstr,batchtype,sets):
+    uidlist = np.loadtxt(fpath + 'uidlist_' + batchtype + "_" + sets + "_" + batchstr, dtype=np.str, delimiter="\t")
+    weibolist = np.loadtxt(fpath + 'weibolist_' + batchtype + "_" + sets + "_" + batchstr, dtype=np.str, delimiter="\t")
+    return uidlist,weibolist
+
+def get_weibolist_by_uid(uid,weibolist):
+    '''
+    根据uid抽取微博
+    '''
+    ts=np.where(weibolist==uid)
+    lts=weibolist[ts[0],1]
+    return lts
+
+def ele_ct_sort(lts):
+    '''
+    Parameters
+    ----------
+    #无序微博列表 lts 是 numpy.ndarray
+    
+    Returns
+    -------
+    eles 排序后的元素列表
+    ele_ct 每个元素的个数
+    total_ct 总的微博数
+    bin_ct 元素的个数
+    '''
+    eles,cts=np.unique(lts,  return_counts=True)
+    idx=np.argsort(-cts)
+    return eles[idx],cts[idx],len(lts),len(idx)
+
+def save2file(fpath, batchstr, batchtype, sets, uidlist, weibolist):
+    fout = open(fpath + "uid_city_" + batchtype + "_" + sets + "_" + batchstr, "wb")
+    for uid in uidlist: #获取无序微博列表 lts 是 numpy.ndarray
+        lts = get_weibolist_by_uid(uid, weibolist) #统计,并按从大到小顺序排序
+        eles, ele_ct, total_ct, bin_ct = ele_ct_sort(lts)
+        fout.write(str(uid) + "\t")
+#         fout.write(str(total_ct) + "\t")
+#         fout.write(str(bin_ct) + "\t")
+        for ele_id in range(len(eles)):
+            fout.write(str(eles[ele_id]))
+        fout.write("\n")
+    fout.close()
+
+def save2file_bin(fpath, batchstr, batchtype, sets, uidlist, weibolist):
+    fout = open(fpath + "uid_city_bin" + batchtype + "_" + sets + "_" + batchstr, "wb")
+    for uid in uidlist: #获取无序微博列表 lts 是 numpy.ndarray
+        lts = get_weibolist_by_uid(uid, weibolist) #统计,并按从大到小顺序排序
+        eles, ele_ct, total_ct, bin_ct = ele_ct_sort(lts)
+        fout.write(str(uid) + "\t")
+        fout.write(str(total_ct) + "\t")
+        fout.write(str(bin_ct) + "\t")
+        for ele_id in range(len(eles)):
+            fout.write(str(eles[ele_id])+"\t")
+            fout.write(str(ele_ct[ele_id])+"\t")
+        fout.write("\n")
+    fout.close()
+ 
+
+if __name__ == '__main__':
+    fpath = "/Users/alibaba/Documents/workspace/python/pytest/doubanprocess/src/weibo/data/"
+    batchstr = "3"
+    batchtype = "half"
+    sets_a = "a"
+    sets_b = "b"
+    uidlist_a,weibolist_a=loaddata(fpath, batchstr, batchtype,sets_a)
+    uidlist_b,weibolist_b=loaddata(fpath, batchstr, batchtype,sets_b)
+#     save2file(fpath, batchstr, batchtype, sets_a, uidlist_a, weibolist_a)
+#     save2file(fpath, batchstr, batchtype, sets_b, uidlist_b, weibolist_b)
+    save2file_bin(fpath, batchstr, batchtype, sets_a, uidlist_a, weibolist_a)
+    save2file_bin(fpath, batchstr, batchtype, sets_b, uidlist_b, weibolist_b)
+        
